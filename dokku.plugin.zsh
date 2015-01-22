77d8831c02723fded21d0e9502f9bf853adb4673
rummik <r@9k1.us>
2015-01-22 10:54:50 -0500

    Cache dokku commands

--- dokku.plugin.zsh
+++ dokku.plugin.zsh
@@ -1,15 +1,17 @@
 #!/bin/zsh
 
 DOKKU_HOST=app.9k1.us
+typeset -A DOKKU_COMMANDS
 
 function dokku {
 	emulate -L zsh
 	local app
-	local cmd=$1
+	local cmd=${1:-help}
 	local remote=dokku@$DOKKU_HOST
 	local init=0
+	local host=$DOKKU_HOST
 
-	if [[ ! -z $cmd ]]; then
+	if [[ ! -z $1 ]]; then
 		shift
 	fi
 
@@ -19,10 +21,15 @@ function dokku {
 		if [[ ! -z $gitremote ]]; then
 			app=${gitremote##*:}
 			remote=${gitremote%:*}
+			host=${remote##*@}
 			init=1
 		else
 			app=$(basename $(git rev-parse --show-toplevel))
 		fi
+	fi
+
+	if [[ -z $DOKKU_COMMANDS[$host] ]]; then
+		_dokku-reload-commands
 	fi
 
 	if [[ $cmd == 'init' ]]; then
@@ -31,7 +38,7 @@ function dokku {
 		else
 			print 'Dokku remote exists.'
 		fi
-	elif [[ $init = 1 && ! -z $app && ! -z $cmd && ! -z $(_dokku-ssh help | grep -Po "$cmd <app>") ]]; then
+	elif [[ $init = 1 && ! -z $app && ! -z $cmd && ! -z ${${DOKKU_COMMANDS[$host]}[(r)$cmd]} ]]; then
 		_dokku-ssh $cmd $app $@
 	else
 		_dokku-ssh $cmd $@
@@ -40,6 +47,10 @@ function dokku {
 
 function _dokku-ssh {
 	ssh -t $remote $@
+}
+
+function _dokku-reload-commands {
+	DOKKU_COMMANDS[$host]=$(_dokku-ssh | tail -n +3 | grep -Po '[^ ]+(?= <app>)')
 }
 
 function -dokku-init {
